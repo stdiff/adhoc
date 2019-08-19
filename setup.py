e169fc6ea5f49e1ba2f192193e2f4790c95efcba
Hironori <stdiff@outlook.de>
2019-08-19 21:10:21 +0200

    setup.py init

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="adhoc",
+    version="0.1",
+    description="Module for ad hoc analysis",
+    url="https://github.com/stdiff/adhoc",
+    author="Hironori Sakai",
+    author_email="crescent.lab@gmail.com",
+    license="MIT",
+    packages=["adhoc"],
+    python_requires=">=3.5",
+    install_requires = ["numpy>=1.16.4",
+                        "pandas>=0.23.4",
+                        "scipy>=1.2.0",
+                        "matplotlib>=3.0.2",
+                        "seaborn>=0.9.0",
+                        "scikit-learn>=0.21.3",
+                        "pydot>=1.4.1",
+                        "jupyter>=1.0.0"
+                        ],
+    zip_safe=False
+)
