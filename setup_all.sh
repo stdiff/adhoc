f60e1619bbe2a46c62aa39393ff7213f486571be
Hironori <stdiff@outlook.de>
2020-06-14 17:56:32 +0200

    git and dvc init added

--- setup_all.sh
+++ setup_all.sh
@@ -5,6 +5,9 @@
 ##
 ## The first argument is recognised as the name of virtualenv.
 
+WITH_GIT="yes"
+WITH_DVC="yes"
+
 if [[ -z "$1" ]]; then
   VENV="venv"
 else
@@ -32,16 +35,55 @@ echo "-- Install jupyterlab_spellchecker"
 jupyter labextension install @ijmbarr/jupyterlab_spellchecker
 
 echo "-- Install jupyterlab_templates"
-pip install jupyterlab_templates
+pip install jupyterlab_templates==0.2.3
 jupyter labextension install jupyterlab_templates
 jupyter serverextension enable --py jupyterlab_templates
 
+#echo "-- Install jupyterlab-emacskeys"
+#jupyter labextension install jupyterlab-emacskeys
+
 echo "-- Install further libraries"
 pip install watermark==2.0.2
+pip install click==7.1.2
 pip install scikit-image==0.16.2 pillow==6.2.2
 
 echo "-- create requirements.txt"
 pip freeze > requirements.txt
+
+if [ "$WITH_GIT" == "yes" ]; then 
+echo "-- initialize git"
+git init
+cat << EOF > .gitignore
+## virtualenv
+venv/
+
+## PyCharm
+.idea
+
+## Jupyter Notebook Checkpoints
+notebooks/.ipynb_checkpoints/
+
+## temporary workspace
+tmp/
+EOF
+echo "Do not forget to add a remote: git remote add origin URL"
+git add .gitignore
+git add requirements.txt
+git commit -m "init"
+
+    if [ "$WITH_DVC" == "yes" ]; then 
+    echo "-- install DVC as a Python library"
+    pip install dvc[s3]
+
+    echo "-- initialize DVC"
+    dvc init --quiet
+    dvc install 
+
+    git add .dvc/config
+    git commit -m "DVC prepared"
+    echo "Do not forget to add a remote: dvc remote add -d default URL"
+    fi
+fi
 
 echo "-- Deactivate the Python environment"
 deactivate
